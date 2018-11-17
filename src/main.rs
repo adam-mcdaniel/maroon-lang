94db98076ffb1b3047e281ce961c10eb82f5e2dc
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2018-11-17 01:16:48 -0500

    Cleaned up entry point

--- src/main.rs
+++ src/main.rs
@@ -23,56 +23,5 @@ fn main() {
         Evaluator::new(&preprocessor.process(&line), &line).eval();
     }
 
-    // succ := n.(f.(x.(f[n[f][x]])))
-
-    // println!("{}",
-    //     to_primitive_call(
-    //         "a.(b.b) [0][1]"
-    //     )
-    // );
-
-    // concurrent
-    // info(
-    //     thread::spawn(|| {
-    //         Evaluator::new(
-    //             "a.(a + 1) (fun.(function fun !)) !"
-    //         ).eval().join(" ")
-    //     }).join().unwrap()
-    // );
-    // info(
-    //     thread::spawn(|| {
-    //         Evaluator::new(
-    //             "a.(a !) (x.( 8 c.c x ! )) !"
-    //         ).eval().join(" ")
-    //     }).join().unwrap()
-    // );
-    // info(
-    //     thread::spawn(|| {
-    //         Evaluator::new(
-    //             "28 (a.(a + 1)) !"
-    //         ).eval().join(" ")
-    //     }).join().unwrap()
-    // );
-
-    // procedural
-
-    // T = (a.b.a)
-    // F = (a.b.b)
-    // and = (p.q.(  (p q !) p ! ))
-
-    // debug(&to_primitive_call(
-    //     "(a.(b.a)) [0][1]
-
-    //     @STDOUT \\n @STDOUT
-    //     "));
-
-    // println!("len: {}", "(a.(a a !) b.(b b !) !)".len());
-    // Evaluator::new(
-    //     &to_primitive_call(
-    //         "(a.(a a !) b.(b b !) !)"
-    //     )).eval().join("");
-
-    // info(Evaluator::new("a.(a tion) (fun.(func fun !)) !").eval().join(""));
-    // info(Evaluator::new("a.(a !) (x.( 8 c.c x ! )) !").eval().join(""));
-    // info(Evaluator::new("28 (a.(a 1)) !").eval().join(""));
+    // TODO: Add import function / keyword
 }
