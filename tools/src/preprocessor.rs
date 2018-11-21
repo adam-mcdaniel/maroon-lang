b59dcb4942570aef248514fb1fe20f8cc598092f
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2018-11-21 14:42:34 -0500

    Fixed Add function

--- tools/src/preprocessor.rs
+++ tools/src/preprocessor.rs
@@ -50,7 +50,8 @@ impl Preprocessor {
         p.process("Break = Break_A.(@break)");
 
         p.process("Succ = Succ_N.Succ_F.Succ_X.( Succ_F[Succ_N&[Succ_F&][Succ_X&] ] )");
-        p.process("Add = Plus_M.Plus_N.Plus_F.Plus_X.( Plus_M&[Plus_F][Plus_N&[Plus_F&][Plus_X&]] )");
+        p.process("Add = Plus_M.Plus_N.Plus_F.Plus_X.( Plus_M[Plus_F][Plus_N[Plus_F][Plus_X]] )");
+        // p.process("Add = Plus_M.Plus_N.Plus_F.Plus_X.( Plus_M&[Plus_F][Plus_N&[Plus_F&][Plus_X&]] )");
 
         p.process("Pred = Pred_N.(Pred_N @pred)");
         // p.process("Mult = Mult_M.Mult_N.Mult_F.Mult_X.( Mult_M[Mult_N[Mult_F]][Mult_X])");
